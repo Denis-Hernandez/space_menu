eeb5dde224f153b3dab8834236dc1888b59600ef
Denis Hernandez <kandor95@gmail.com>
2021-11-24 11:16:44 -0600

    este es el primer commit

--- main_view/main_view.js
+++ main_view/main_view.js
@@ -6,3 +6,17 @@ function footer_onClick(value){
     icon.textContent = 'expand_less';
   }
 }
+
+async function requestData(space_object ){
+  const data = await fetch('http://localhost:3000/api/v1/planets/'+space_object.id)
+  .then(response => response.json());
+
+  let central_panel = document.getElementsByClassName("solar-system__central-panel__text")[0];
+  central_panel.innerHTML = "";
+  let content="";
+  console.log(Object.keys(data));
+  Object.keys(data).forEach((key)=>{
+    content += key + '<dd>' +data[key] + "</dd>";
+  });
+  central_panel.innerHTML = content;
+}
